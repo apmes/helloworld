19cf7b590165b4e9f3a596361c16383e62f8b0b9
Alvaro <Alvaro@APMDELL>
2019-01-11 12:32:39 +0100

    refactor class

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -3,8 +3,11 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java World!");
-
+		HelloWorld hw = new HelloWorld();
+		hw.printMessage();
 	}
 
+	private void printMessage() {
+		System.out.println("Hello Java World!");
+	}
 }
